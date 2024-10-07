58166e382e5b147495b16b4391dd30c03cc6fd36
TechbroMoh <felixmonari400@gmail.com>
2024-10-07 05:28:10 +0300

    Implement basic Flask application for pet directory

--- server/app.py
+++ server/app.py
@@ -9,6 +9,7 @@ from models import db, Pet
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -16,16 +17,18 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
-
+    body = {'message': 'Welcome to the pet directory!'}
+    return make_response(body, 200)
 
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+    pet = Pet.query.first()
+    pet_dict = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species
+                }
+
+    return make_response(pet_dict, 200)
 
 
 if __name__ == '__main__':
